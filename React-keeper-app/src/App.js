1348eab02d22e03b6219f5c4274545980799864f
EmeraudeFleur <82100635+EmeraudeFleur@users.noreply.github.com>
2021-11-06 12:14:27 +0100

    Add files via upload

---
+++ React-keeper-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import notes from "./notes";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
+      <Footer />
+    </div>
+  );
+}
