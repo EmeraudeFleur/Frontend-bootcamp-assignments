52ed2f895360c61162603c60c7c50a76f6f9f99f
EmeraudeFleur <82100635+EmeraudeFleur@users.noreply.github.com>
2021-11-06 19:48:16 +0100

    Add files via upload

---
+++ React-keeper-app-final/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
+
+export default function App() {
+  const [notes, setNotes] = useState([]);
+  function addNote(note) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+
+      <Footer />
+    </div>
+  );
+}
